32017d198199a1c17f21b364cb896dff89c42928
reggae93 <35476328+reggae93@users.noreply.github.com>
2020-05-21 23:57:36 -0300

    Add files via upload

---
+++ multiplicación.py
@@ -0,0 +1,10 @@
+print("Ingreso De dos Numeros y su multiplicación")
+n1=int(input("Ingrese el Primer Numero: "))
+n2=int(input("Ingrese el Segundo Numero: "))
+
+multiplicación = n1*n2;
+
+print("La multiplicación es : " + str(multiplicación))
+
+print("Felicidades Multiplico los dos numeros correctamente :D")
+
